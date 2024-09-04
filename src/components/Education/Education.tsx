95862d77799cae6705d22813eedea6c3d4237bbe
Inna-Kulish <norelajest@gmail.com>
2024-09-04 22:07:05 +0300

    education section

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import styles from "./Education.module.scss";
-import Hat from "../../assets/icons/education/study.svg?react";
+import Hat from "@/assets/icons/education/study.svg?react";
 import Title from "../Title/Title";
+import women from "@/assets/images/education/women.jpg";
+import goit from "@/assets/images/education/goit.jpg";
 
 const Education: React.FC = () => {
   return (
     <section className={styles.education}>
       <Title title="My" span="Education" Icon={Hat} color="light" />
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <img className={styles.photo} src={women} alt="Women go tech" />
+          <div className={styles.content}>
+            <h3 className={styles.course}>Software Development & Quality Assurance</h3>
+            <p className={styles.school}>Women Go Tech Acceleration Program</p>
+            <p className={styles.year}>2024 | Pursuing</p>
+          </div>
+        </li>
+        <li className={styles.item}>
+          <img className={styles.photo} src={goit} alt="Go IT"/>
+          <div className={styles.content}>
+            <h3 className={styles.course}>Fullstack developer</h3>
+            <p className={styles.school}>course at GoIT school</p>
+            <p className={styles.year}>2022-2023 | Completed</p>
+            </div>
+        </li>
+      </ul>
     </section>
   );
 };
