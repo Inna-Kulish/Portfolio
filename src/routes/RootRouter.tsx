db932ec1b9ef3322376d30610c1dc7103605b180
Inna-Kulish <norelajest@gmail.com>
2024-09-15 17:36:26 +0300

    add animation

--- src/routes/RootRouter.tsx
+++ src/routes/RootRouter.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Projects from "../pages/Projects/Projects";
 
 const RootRouter: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects/>} />
     </Routes>
   );
 };
