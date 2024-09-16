888e9fad3fcf53187a02804ab563aff06fcbc724
Inna-Kulish <norelajest@gmail.com>
2024-09-16 22:02:35 +0300

    footer section

--- src/routes/RootRouter.tsx
+++ src/routes/RootRouter.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Projects from "../pages/Projects/Projects";
+import SharedLayout from "../components/SharedLayout/SharedLayout";
 
 const RootRouter: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects/>} />
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Route>
     </Routes>
   );
 };
