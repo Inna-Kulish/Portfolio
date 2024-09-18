ac63a0ef316350d24bb2581398ca68a12e0593a7
Inna-Kulish <norelajest@gmail.com>
2024-09-18 23:20:27 +0300

    mobile menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "@/assets/icons/header/logo.svg?react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
+import Menu from "@/assets/icons/header/menu.svg?react";
+import Cross from "@/assets/icons/header/cross.svg?react";
 import { NAV_LINKS, SectionIds } from "./navList";
 
 const Header: React.FC = () => {
-    
+  const [isMenuTooggled, setIsMenuToggled] = useState<boolean>(false);
+
   const scrollToSection = (sectionId: SectionIds) => {
     const element = document.getElementById(sectionId);
     if (element) {
@@ -32,6 +35,32 @@ const Header: React.FC = () => {
           ))}
         </ul>
       </nav>
+
+      {/* Mobile menu */}
+      <div className={styles.mobileContainer}>
+        <button
+          className={styles.menuBtn}
+          onClick={() => setIsMenuToggled(!isMenuTooggled)}
+        >
+          {isMenuTooggled ? <Cross className={styles.mobileIcon}/> : <Menu width={28} height={32} />}
+        </button>
+      </div>
+      {isMenuTooggled && (
+        <div className={styles.mobileBox}>
+          <ul className={styles.mobileList}>
+            {NAV_LINKS.map(({ title, sectionId }, index) => (
+              <li key={`${title}_${index}`} className={styles.itemMobile}>
+                <a
+                  className={styles.linkMobile}
+                  onClick={() => scrollToSection(sectionId)}
+                >
+                  {title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </header>
   );
 };
